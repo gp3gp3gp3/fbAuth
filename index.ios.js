4d944bde8701e1bd310f8ba7ff802ff3641c55f1
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-12-08 11:31:15 -0500

    Add facebook login button

--- index.ios.js
+++ index.ios.js
@@ -12,6 +12,39 @@ import {
   View
 } from 'react-native';
 
+const FBSDK = require('react-native-fbsdk');
+const {
+  LoginButton,
+  AccessToken
+} = FBSDK;
+
+var Login = React.createClass({
+  render: function() {
+    return (
+      <View>
+        <LoginButton
+          publishPermissions={["publish_actions"]}
+          onLoginFinished={
+            (error, result) => {
+              if (error) {
+                alert("login has error: " + result.error);
+              } else if (result.isCancelled) {
+                alert("login is cancelled.");
+              } else {
+                AccessToken.getCurrentAccessToken().then(
+                  (data) => {
+                    alert(data.accessToken.toString())
+                  }
+                )
+              }
+            }
+          }
+          onLogoutFinished={() => alert("logout.")}/>
+      </View>
+    );
+  }
+});
+
 export default class fbAuth extends Component {
   render() {
     return (
@@ -26,6 +59,7 @@ export default class fbAuth extends Component {
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
         </Text>
+        <Login />
       </View>
     );
   }
